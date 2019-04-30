8178d93a6a435592ca17e0871aa9d0a8c726a7fa
Carlos Albuquerque Carneiro <carlos@Carlos-Albuquerque-Carneiro.local>
2019-04-30 00:22:54 -0300

    article crud

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, except: %i[new index create]
+
+  def new
+    @article = Article.new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @article.update(articles_params)
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+
+  def show; end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = 'Article was successfully destroyed'
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def articles_params
+    params.require(:article).permit(:title, :description)
+  end
+end
