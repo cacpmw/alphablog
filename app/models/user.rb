51b04c64383003747ef7ea30a1c9c57ff80956e6
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2019-05-01 12:46:16 -0300

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
 
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
+                       length: { minimum: 3, maximum: 15 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true,
+                    length: { minimum: 10, maximum: 300 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
