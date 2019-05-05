3cfd8ae75870c7c97a9ae98c612855ab46668de0
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2019-05-04 22:20:19 -0300

    gitignore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, except: %i[new index create]
-  before_action :required_user, except: %i[index show]
+  before_action :required_user, only: %i[update destroy]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to users_path
+      redirect_to users_path(@user)
     else
       render 'new'
     end
