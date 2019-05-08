ada1a20cf576cd0e9669b889bf9d481acb41310b
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2019-05-08 09:14:38 -0300

    welcome controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def required_user
     if !logged_in?
-      flash[:danger] = "You must be logged in to perform that action"
+      flash[:danger] = 'You must be logged in to perform that action'
       redirect_to root_path
     end
   end
